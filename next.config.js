0a4e881c82f2b9657dd5fc6b101869d37f2b44a4
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-08-24 11:20:11 -0700

    working next-mdx-remote

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  webpack5: (config, { isServer }) => {
+    // console.log({ config });
+    // Fixes npm packages that depend on 'fs' module
+    if (!isServer) {
+      config.node = { fs: 'empty' };
+    }
+    return config;
+  },
   reactStrictMode: true,
-}
+};
--- src/pages/[post].tsx
+++ src/pages/[post].tsx
@@ -1,11 +1,67 @@
+import fs from 'fs';
+import path from 'path';
+import { ReactNode } from 'react';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import matter from 'gray-matter';
+import { serialize } from 'next-mdx-remote/serialize';
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
+import { Heading, Text } from '@chakra-ui/layout';
 
-const Post = () => {
-  return <div>Test</div>;
+
+import TestComp from '../components/TestComp';
+
+type PostProps = {
+  source: MDXRemoteSerializeResult;
+  metaInformation: {
+    seoTitle: string
+  };
+};
+
+const H1 = (props: { children: ReactNode }) => {
+  return (
+    <Heading as="h1" size="lg">
+      {props.children}
+    </Heading>
+  );
+};
+
+const P = (props: { children: ReactNode}) => {
+  return (
+    <Text as="p" size="md">{props.children}</Text>
+  );
+};
+
+const components = {
+  h1: H1,
+  p: P,
+  TestComp
+};
+
+const Post = (props: PostProps) => {
+  return (
+    <>
+      <div>
+        <MDXRemote {...props.source} components={components} />
+      </div>
+      <div>SEO Information: {JSON.stringify(props.metaInformation)}</div>
+    </>
+  );
 };
 
 export const getStaticProps: GetStaticProps = async (props) => {
-  return { props: {} };
+  const folderPath = path.join(process.cwd(), 'src/content');
+  const params = props.params!.post;
+  const filePath = path.join(folderPath, `${params}.mdx`);
+  const rawFileSource = fs.readFileSync(filePath);
+  const { content, data } = matter(rawFileSource);
+  const mdxSource = await serialize(content);
+
+  return {
+    props: {
+      source: mdxSource,
+      metaInformation: data
+    }
+  };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
