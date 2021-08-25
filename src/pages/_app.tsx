c1f36ddab393220fc94e35141bae3c3bd782d040
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-08-24 22:34:14 -0700

    moved Header to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import customTheme from '../styles/customTheme';
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={customTheme}>
+      <Header />
       <Component {...pageProps} />
     </ChakraProvider>
   );
