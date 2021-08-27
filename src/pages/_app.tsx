f26ca059228a9eecf35f2a5458960507ee0890ac
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-08-26 22:51:33 -0700

    font awesome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,11 @@ import { ChakraProvider } from '@chakra-ui/react';
 import customTheme from '../styles/customTheme';
 import Header from '../components/sections/Header';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCopy } from '@fortawesome/free-regular-svg-icons';
+
+library.add(faCopy);  
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={customTheme}>
